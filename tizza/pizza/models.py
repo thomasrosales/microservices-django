4bce8c7efdb2a6459b6d6caaa257fc1b5747dbec
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-09-13 17:56:21 -0300

    user app added

--- tizza/pizza/models.py
+++ tizza/pizza/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    title = models.CharField(max_length=120, unique=True)
+    description = models.CharField(max_length=240)
