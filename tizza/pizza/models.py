93050428a6e1a52ace0607e0e56b7568a16dcf79
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-09-13 18:00:50 -0300

    pizza model

--- tizza/pizza/models.py
+++ tizza/pizza/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from user.models import UserProfile
+
 
 # Create your models here.
+
+
+class Pizzeria(models.Model):
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=40)
+
 
 class Pizza(models.Model):
     title = models.CharField(max_length=120, unique=True)
     description = models.CharField(max_length=240)
+    thumbnail_url = models.URLField()
+    approved = models.BooleanField(default=False)
+    creator = models.ForeignKey(Pizzeria, on_delete=models.CASCADE)
+
+
+class LikeS(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
