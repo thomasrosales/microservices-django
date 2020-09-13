2820dd1dc92285feb352db4ddcdbff68bcc900a3
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-09-13 18:03:23 -0300

    migrations and settings add the new user app

--- tizza/tizza/settings.py
+++ tizza/tizza/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pizza.apps.PizzaConfig'
+    'pizza.apps.PizzaConfig',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
